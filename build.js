79eb44c370949209f258a3add883a98ada224bd9
Anton <dev@atjn.dk>
2024-06-07 13:41:19 +0200

    Upgrade Firefox to manifest v3

--- build.js
+++ build.js
@@ -4,16 +4,6 @@ import AdmZip from "adm-zip";
 await fs.emptyDir("public/firefox/");
 await fs.copy("source/", "public/firefox/");
 
-const manifest = await fs.readJson("public/firefox/manifest.json");
-manifest.manifest_version = 2;
-const links = [];
-for (const resource of manifest.web_accessible_resources) {
-	for (const link of resource.resources) {
-		links.push(link);
-	}
-}
-manifest.web_accessible_resources = links;
-await fs.writeJson("public/firefox/manifest.json", manifest, { spaces: "\t" });
 await generateZip("public/firefox/", "public/firefox.zip");
 
 await fs.emptyDir("public/chrome/");
